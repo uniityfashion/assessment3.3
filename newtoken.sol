eaa52781d5f437d1f6572dc58ef2063f678db79f
uniityfashion <139683920+uniityfashion@users.noreply.github.com>
2024-07-01 05:09:59 +0530

    Update newtoken.sol

--- newtoken.sol
+++ newtoken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
 contract newtoken is ERC20 {
     address public owner;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         owner = msg.sender;
-        _mint(msg.sender, 10000);
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
     function mint(address account, uint256 amount) public {
